9e153522ecaeb1b4a5ba3c53cedb34a6405724b4
igor <igor.vitor@portalsesisp.org.br>
2024-02-07 11:26:39 -0300

    Js basico

---
+++ 01-variaveis.js
@@ -0,0 +1,13 @@
+//criando variaveis com var (variavel)
+var nome = "igor"
+var idade = 17
+
+ 
+console.log("meu nome é", nome, "e tenho", idade, "anos")
+
+
+//console.log("agora fiz aniversario tenho", idade+1)
+
+//aumentando automaticamente mais 1 (autoincremento)
+idade++
+console.log("agora fiz aniversario tenho", idade)
---
+++ 02-aritimetico.js
@@ -0,0 +1,22 @@
+//utilizando operadores aritimeticos com javaScript
+
+console.log("soma entre 8 e 7 é:", 8 + 7);
+//efetuar a soma de dois numeros informados por você (var nome variavel = 12)
+var x = 10;
+var y = 5;
+
+console.log("a soma das variaveis", x, "e", y, "é:", x + y);
+
+var soma = x + y
+var subtracao = x - y
+var mutiplicacao = x * y
+var divisao = x / y
+
+console.log("soma = ",soma)
+console.log(" subtracao = ",subtracao)
+console.log("mutiplicacao = ",mutiplicacao)
+console.log("divisao =",divisao)
+
+
+//Regras para nomear identivicador (var const ou let)
+// podem começar com a-z,_ ou _ $
